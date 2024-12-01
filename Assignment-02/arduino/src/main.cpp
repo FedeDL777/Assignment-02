ea34823d1c123ef20b332aff3937f1a7cecc2b4b
Federico De Leonardis <federico.deleonardis@studio.unibo.it>
2024-12-01 21:02:04 +0100

    Create main

--- Assignment-02/arduino/src/main.cpp
+++ Assignment-02/arduino/src/main.cpp
@@ -1,18 +1,52 @@
+
 #include <Arduino.h>
+#include "set-up.h"
+#include "core/Scheduler.h"
+#include "core/MsgService.h"
+#include "core/Logger.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#include "SWDSPlant.h"
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+#include "tasks/api/DetectorTask.h"
+#include "tasks/api/ServoTask.h"
+#include "tasks/api/LedTask.h"
+#include "tasks/api/PirTask.h"
+#include "tasks/api/TempTask.h"
+
+Scheduler sched;
+
+SWDSPlant* machine;
+
+void setup(){
+  MsgService.init();
+  sched.init(100);
+
+  machine = new SWDSPlant();
+  machine->init();
+
+  Task* pDetectorTask = new DetectorTask(machine);
+  pDetectorTask->init(100);
+
+  Task* pServoTask = new ServoTask(machine);
+  pServoTask->init(200);
+
+  Task* pLedTask = new LedTask(machine);
+  pLedTask->init(100);
+
+  Task* pPir = new PirTask(machine);
+  pPir->init(100);
+
+  Task* pTempTask = new TempTask(machine);
+  pTempTask->init(100);
+  //
+
+//  sched.addTask(pServoTask);
+//  sched.addTask(pLedTask);
+  sched.addTask(pPir);
+
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop(){
+  sched.schedule();
 }
